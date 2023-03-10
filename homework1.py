46485070c4e3dcfc9301e4b2e30f114a8c0bb676
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2023-03-10 17:31:39 +0300

    first homework done

---
+++ homework1.py
@@ -0,0 +1,22 @@
+egitmen = "Engin Demirog"
+kategori = "Programlama"
+tamamlanmaYuzdesi = 50
+kursTamamlandıMı = True
+
+if tamamlanmaYuzdesi == 100:
+    print("kursu tamamladınız! , tebrikler!")
+elif tamamlanmaYuzdesi >= 50:
+    print("kursu yarıladınız, devam edin!")
+else:
+    print("kurs henüz başlamadı / henüz aktif olaeak katılımda bulunmadınız")
+
+# Python'daki veri tipleri
+
+# Integer : Tam sayı veri tipidir.
+# String : Metin veri tipidir
+# Bool : Mantıksal veri tipidir true false döner
+# Double : Hassas matematiksel işlemler için iyidir , ondalık sayı veri tipi
+# Float : Basit ondalık sayı veriler içindir
+# List : Arraylere benzer, farklı veri tipleri barındırabilirler.[]
+# Tuples : Listeye benzer. güncellenemezler (read-only) ()
+# Dict : Bir çeşit hash tablo türü., anahtar-değer çiftleriden oluşurlar.{}ile tanımlanıp , [] ile erişilir.
